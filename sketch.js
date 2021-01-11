ff864d579e0711c0d2ca4f2deeb877225065468d
Jofeena <71768181+Jofeena@users.noreply.github.com>
2021-01-11 15:25:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var particle, plinko, division, ground;
+
+function setup() {
+  createCanvas(480, 800);
+  //createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(0);  
+
+  drawSprites();
+}
