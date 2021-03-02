a0c75605ddb0608fbf776211393ccc0dbdbdb622
Jofeena <71768181+Jofeena@users.noreply.github.com>
2021-03-02 13:26:04 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,213 +1,125 @@
-var Engine = Matter.Engine,
-  World = Matter.World,
-  Events = Matter.Events,
-  Bodies = Matter.Bodies;
- 
-var particles
-var plinkos = [];
+//Namespacing
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Body=Matter.Body;
+const Constraint = Matter.Constraint;
+
+//Variables
+var particles=[];
+var plinkos1=[];
+var plinkos2=[];
+var plinkos3=[];
+var plinkos4=[];
 var divisions=[];
 
 var divisionHeight=300;
-var score =0;
-var turn=0;
 
-const PLAY=1;
-const END=0;
-var gameState=PLAY;
-
-var bg;
-var txtcolor;
-
-function preload()
-{
-  bg=color("black");
-  txtcolor=color("black");
-  fetchtime();
-}
+var upground,leftground,rightground;
+var bottomdivision;
 
 function setup() {
-  createCanvas(800, 800);
+  //To create canvas
+  createCanvas(480,800);
+
   engine = Engine.create();
   world = engine.world;
-  ground = new Ground(width/2,height,width,20);
 
-  var abc=Math.round(random(1,10));
-  abc=abc*50
-  
-  console.log(abc);
+  //Creating bottom division using ground class
+  bottomdivision=new Ground(240,795,width,10);
 
-   for (var k = 0; k <=width; k = k + 80) {
-     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-   }
-
-
-    for (var j = 75; j <=width; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,75));
-    }
-
-    for (var j = 50; j <=width-10; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,175));
-    }
-
-     for (var j = 75; j <=width; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,275));
-    }
-
-     for (var j = 50; j <=width-10; j=j+50) 
-    {
-    
-       plinkos.push(new Plinko(j,375));
-    }
-
-    
-
-    
-}
- 
-
-
-function draw() {
-  background(bg);
-  textSize(20)
- //text("Score : "+score,20,30);
-  Engine.update(engine);
- 
-  //console.log(score);
-   for (var i = 0; i < plinkos.length; i++) {
-     
-     plinkos[i].display();
-     
-   }
-  /* if(frameCount%60===0){
-     particles=new Particle(random(width/2-30, width/2+30), 10,10)
-     score++;
-   }*/
- 
- 
-   for (var k = 0; k < divisions.length; k++) {
-     
-     divisions[k].display();
-   }
-
-   //To display the score
-
-   textSize(30);
-   fill(txtcolor)
-   text("Score : "+score,20,35);
-
-   if(particles!=null)
+   //Creating multiple divisions using for loop
+   for(var d=0; d<=width; d=d+80)
    {
-     particles.display();
-
-     if(particles.body.position.y>760)
-     {
-       if(particles.body.position.x<300)
-       {
-         score=score+500;
-         //particles=null;
-         if(turn===5)
-         {
-           gameState=END;
-         }
-       }
-      
-  
-       if(particles.body.position.x>301&&particles.body.position.x<600)
-       {
-          score=score+100;
-         // particles=null;
-          if(turn===5)
-          {
-            gameState=END;
-          }
-       }
-
-       if(particles.body.position.x>601&&particles.body.position.x<900)
-       {
-         score=score+200;
-        // particles=null;
-         if(turn===5)
-         {
-           gameState=END;
-         }
-       }
-       particles=null;
-
-     }
+     divisions.push(new Divisions(d,height-divisionHeight/2,10,divisionHeight));
    }
-
-   if(gameState===END)
-   {
-     push();
-     strokeWeight(1);
-     stroke("red")
-     textSize(60);
-     fill(txtcolor)
-     text("GAME OVER",200,250);
-     textSize(50);
-     stroke("yellow")
-     text("Press Space Key to Restart",100,340)
-     pop();
-   }
-
-   fill(txtcolor)
-   textSize(32);
-   text("500",15,550);
-   text("500",95,550);
-   text("500",95+80,550);
-   text("100",95+160,550);
-   text("100",95+240,550);
-   text("100",95+240+80,550);
-   text("100",95+240+160,550);
-   text("200",95+320+160,550);
-   text("200",95+320+240,550);
-   text("200",95+320+320,550);
 
    
+  //Creating first line of multiple plinkos
+  for(var a=40; a<=width; a=a+50)
+  {
+    plinkos1.push(new Plinko(a,75))
+  }
 
+  //Creating second line of multiple plinkos
+  for(var b=15; b<width-10; b=b+50)
+  {
+    plinkos2.push(new Plinko(b,175));
+  }
+
+  //Creating third line of multiple plinkos
+  for(var c=40; c<width; c=c+50)
+  {
+    plinkos3.push(new Plinko(c,275));
+  }
+
+  //Creating fourth line of multiple plinkos
+  for(var d=15; d<width-10; d=d+50)
+  {
+    plinkos4.push(new Plinko(d,375));
+  }
+
+ upground=new Ground(width/2,2.5,width,5);
+  leftground=new Ground(1,height/2,5,height);
+ rightground=new Ground(479.5,height/2,5,height);
+ 
+}
+
+function draw() {
+  //black background
+  background(0);  
+
+  Engine.update(engine);
+
+  //displaying ground
+  bottomdivision.display();
+
+ 
+
+  //Displaying divisions
+  for(var z=0; z<=divisions.length-1; z=z+1)
+  {
+    divisions[z].display();
+  }
+
+
+  //Displaying plinkos
+  for(var e=0; e<=plinkos1.length-1; e=e+1)
+  {
+    plinkos1[e].display();
+  }
+
+  for(var e=0; e<=plinkos1.length-1; e=e+1)
+  {
+    plinkos2[e].display();
+  }
+
+  for(var e=0; e<=plinkos1.length-1; e=e+1)
+  {
+    plinkos3[e].display();
+  }
+
+  for(var e=0; e<=plinkos1.length-1; e=e+1)
+  {
+    plinkos4[e].display();
+  }
+
+  //Displaying particles after every 60 frames
+  if(frameCount%60===0)
+  {
+    particles.push(new Particle(random(width/2-10,width/2+10),10,10));
+  }
   
-}
-
-
-function mousePressed()
-{
-  if(gameState!==END)
+  //Displaying particles
+  for(var f=0; f<=particles.length-1; f=f+1)
   {
-    turn=turn+1;
-    particles=new Particle(mouseX,10,10,10);
-
+    particles[f].display();
   }
-}
+  
+ 
 
-function keyPressed()
-{
-  if(keyCode===32)
-  {
-    score=0;
-    turn=0;
-    gameState=PLAY;
-  }
-}
-
-async function fetchtime()
-{
-    var time=await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
-    var data=await time.json();
-    console.log(data);
-    var hour=data.datetime.slice(11,13);
-    if(hour>6&&hour<18)
-    {
-      bg=color("white");
-      txtcolor=color("black")
-    }
-    else{
-       bg=color("black")
-       txtcolor=color("white")
-    }
+ 
+  upground.display();
+  leftground.display
+ rightground.display();
 }
